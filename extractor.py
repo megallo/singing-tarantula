27c22131c615581cadb95eb3d7769ea22531cd2a
Megan Galloway <megalloway@gmail.com>
2012-04-29 13:48:07 -0700

    Bahaha found the script that was hiding the content I wanted. Success! Next: dump found things into file

--- extractor.py
+++ extractor.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 
+####
+# Usage: python extractor.py filename
+####
+
+import re
 import os
 import sys
 from bs4 import BeautifulSoup
@@ -15,26 +20,31 @@ count = 1
 #    count += 1
 #    f.close()
 
-def extractLyrics(filename):
-    soup = BeautifulSoup(open(filename))
+def extractLyrics(soup):
     lyrics = soup.find(attrs={'name' : "description"})
     return lyrics['content']
 
-def extractComments(filename, minRating):
-    f = open(filename)
-    unicode(f)
-    soup = BeautifulSoup(f)
-    f = open("sopretty", 'w')
-    f.write(soup.prettify())
-    f.close()
+def extractComments(soup, minRating):
+#    f = open("sopretty", 'w')
+#    f.write(soup.prettify())
+#    f.close()
     print soup.find(id="songText2")
-    commentBlock = soup.find("a",attrs={'name' : "comment"})#find_all("div")#, id="comments_listing")
-    print commentBlock
-    print commentBlock.div['class']
+#    commentBlock = soup.find("a",attrs={'name' : "comment"})#find_all("div")#, id="comments_listing")
+#    print commentBlock
+#    print commentBlock.div['class']
 
 
 def main():
-    extractComments(sys.argv[1], 2)
+    #remove offensive items before handing it to BS
+    #they used some trickery to prevent me from reading the important content
+    #but they can't stop me muahahaha
+    html = open(sys.argv[1]).read()
+    html = html.replace(r'class="protected"','')
+    html = re.sub(r'<scr.*ipt>.*</scr.*ipt>','',html)
+    soup = BeautifulSoup(html)
+
+    print extractLyrics(soup)
+    extractComments(soup, 2)
 
 if __name__ == "__main__":
     main()
