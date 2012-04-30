2c81366d92b855ad730caeb9f3bb30a25c185797
Megan Galloway <megalloway@gmail.com>
2012-04-29 19:29:06 -0700

    Progress, but something is weird and soup won't read the html

--- extractor.py
+++ extractor.py
@@ -6,43 +6,47 @@
 
 import re
 import os
-import string
 import sys
+from traceback import format_exc
 from bs4 import BeautifulSoup
 
 MIN_RATING = -100
 
 class Extractor (object):
 
-    def __init__(self):
+    def __init__(self, soup):
         self.commentcount = 0
+        self.soup = soup
 
-    def extractLyrics(self, soup):
-        lyrics = soup.find(attrs={'name' : "description"})
+    def extractLyrics(self):
+        lyrics = self.soup.find(attrs={'name' : "description"})
         return lyrics['content']
 
     '''
         Expects one song page. Returns all the comments appended to one string
     '''
-    def extractComments(self, soup, minRating):
+    def extractComments(self):
+        returnMe = []
         ## this will get all the top-level comments ##
-        comments = soup.find_all(id=re.compile("comrate-.*"))
+        comments = self.soup.find_all(id=re.compile("comrate-.*"))
         for comment in comments:
             rating = comment.find(id=re.compile("ratecomment-.*"))
-            if int(rating.strong.string) >= minRating:
+            if int(rating.strong.string) >= MIN_RATING:
                 divs = comment.parent.find("div",{"class":"com-floatright"})
                 if divs is not None:
                     self.commentcount += 1
                     for item in divs.previous_siblings: #keep in mind this prints the lines in reverse order
                         if type(item).__name__ =="NavigableString": #omits <br/> tags
-                            print self.commentcount,item.string.strip()
+                            returnMe.append(item.string.strip())
 
         ## this will get all replies ##
-        replies = soup.find_all("div",{"class":"com-subreply-right"})
+        replies = self.soup.find_all("div",{"class":"com-subreply-right"})
         for reply in replies:
-            if int(reply.span.strong.string) >= minRating:
+            if int(reply.span.strong.string) >= MIN_RATING:
                 self.commentcount += 1
-                print self.commentcount,reply.contents[2].string.strip()
+                returnMe.append(reply.contents[2].string.strip())
+
+        return returnMe
 
     def count(self):
         return self.commentcount
@@ -56,20 +60,42 @@ class Extractor (object):
 class SongHandler (object):
 
     def __init__(self, indir, outdir):
+        if not (os.path.isdir(indir) and os.path.isdir(outdir)):
+            raise IOError("Give me directory names")
         self.inputDirectory = indir
         self.outputDirectory = outdir
 
     def handleSong(self):
-        for songfile in os.listdir(self.inputDirectory):
-            print songfile
+        try:
+            # use the artist and song name from the input file
+            outputFileName = os.listdir(self.inputDirectory)[0]
+            outputFileName = re.split(re.compile("__\d+$"),outputFileName)[0]
+            out = open(os.path.join(self.outputDirectory,outputFileName), 'w')
+            print outputFileName
+            needLyrics = True
+            for songfile in os.listdir(self.inputDirectory):
+                songfile = os.path.join(self.inputDirectory, songfile)
+                html = self.cleanHTML(songfile)
+                soup = BeautifulSoup(html)
+                ext = Extractor(soup)
+                if needLyrics:
+                    print ext.extractLyrics()
+                    needLyrics = False
+                print ext.extractComments()
 
-#        #remove offensive items before handing it to BS
-#        #they used some trickery to prevent me from reading the important content
-#        #but they can't stop me muahahaha
-#        html = open(sys.argv[1]).read()
-#        html = html.replace(r'class="protected"','')
-#        html = re.sub(r'<sc.*r.*ipt>.*</sc.*r.*ipt>','',html.lower())
-#        soup = BeautifulSoup(html)
+            out.close()
+        except IndexError:
+            print "WARNING: skipping song because the directory is empty -> %s" % self.inputDirectory
+
+
+    def cleanHTML(self, filename):
+        #remove offensive items before handing it to BS
+        #they used some trickery to prevent me from reading the important content
+        #but they can't stop me muahahaha
+        html = open(filename).read()
+        html = html.replace(r'class="protected"','')
+        return re.sub(r'<sc.*r.*ipt>.*</sc.*r.*ipt>','',html.lower())
+#
 #
 #        #print extractLyrics(soup)
 #        e = Extractor()
@@ -80,9 +106,19 @@ class SongHandler (object):
     #    f = open(subdirname, 'w') # one file per song
 
 def main():
-    # TODO: this should take the top level dir and then handle each song's dir
-    handler = SongHandler(sys.argv[1],sys.argv[2])
-    handler.handleSong()
+    try:
+        topLevelDir = sys.argv[1]
+        output = sys.argv[2]
+        for songDirs in os.listdir(topLevelDir):
+            try:
+                handler = SongHandler(songDirs, output)
+                handler.handleSong()
+            except IOError, error:
+                print "WARNING: skipping %s - maybe it's not a directory?\n%s" % (songDirs, error)
+                print format_exc()
+    except Exception, e:
+        print "Usage: python extractor.py <dir containing song dirs> <output dir>\n %s" % e
+        print format_exc()
 
 if __name__ == "__main__":
     main()
