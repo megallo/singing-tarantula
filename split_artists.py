d6bf47c49af2301f12c73b8c65fe62cca68dc956
Megan Galloway <megalloway@gmail.com>
2012-05-07 18:58:03 -0700

    OK, it makes a bunch of condor files

--- split_artists.py
+++ split_artists.py
@@ -3,6 +3,32 @@
 import os
 import sys
 
+def make_condor_job(executable, args, out = "_stdout", err = "_error", log = "_log", mem = 2):
+    """Contains the skeletons of a condor job running in the vanilla universe. 
+    First argument is for executable, second for arguments, 
+    out for output (defaults to "_out"), err for error (defaults to "_err"), log for log (defaults to "_log"), 
+    and mem is the number of GB of memory needed (defaults to 2)."""
+    # if args is sent as ["1", "2", "3"] then convert it to "1 2 3"
+    if type(args) == list:
+        args = " ".join(args)
+    condor = []
+    # append universe
+    condor.append("universe = vanilla")
+    # append executable
+    condor.append("executable = " + executable)
+    # append arguments
+    condor.append("arguments = " + '"' + args + '"')
+    # append output, error, and log paths
+    condor.append("output = " + out)
+    condor.append("error = " + err)
+    condor.append("log = " + log)
+    #condor.append("request_memory = " + str(mem) + "*1024")
+    condor.append("transfer_executable = false")
+    condor.append("queue")
+
+    return "\n".join(condor) + "\n"
+
+
 def main():
 	
 	if len(sys.argv) < 3:
@@ -13,6 +39,10 @@ def main():
 		fullURL = sys.argv[1] + chr(code)
 		fulloutput = os.path.join(sys.argv[2], chr(code))
 		print fullURL, fulloutput
+		writeme = make_condor_job( executable = "/opt/python-2.7/bin/python2.7", args = "crawler.py " + fullURL + " " + fulloutput)
+		f = open(chr(code) + ".cmd", 'w')
+		f.write(writeme)
+		f.close
 
 
 if __name__ == "__main__":
