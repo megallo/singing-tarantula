dd97d0cc8b884444c32dcb609fab00bf8a7833b3
Megan Galloway <megalloway@gmail.com>
2012-08-21 19:19:36 -0500

    Adding back the .06 compatible versions, since .07 is totally broken

---
+++ java/FuzzyClusterOutput.java
@@ -0,0 +1,153 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.mahout.clustering.WeightedPropertyVectorWritable;
+import org.apache.mahout.math.NamedVector;
+import org.apache.mahout.math.Vector;
+
+/**
+ * Adaptation of cluster writer from Mahout in Action,
+ * but fixed for 0.6 and writes more useful cluster info,
+ * and then re-adapted to work with fuzzy cluster probabilities
+ * 
+ * Output file name is the user-defined prefix_alldocs
+ * 
+ * @author megs
+ */
+public class FuzzyClusterOutput {
+	
+	//holds the name of the point mapped to a list of pairs of cluster-probabilities
+	private HashMap<String, ArrayList<Pair>> pointVectorToClusterProbs = new HashMap<String, ArrayList<Pair>>();
+	
+	private String inputDir;
+
+/**
+ * @param args - <input clusteredPoints dir>  <output file prefix>
+ */
+	public static void main(String[] args) {
+	    try {
+	    	FuzzyClusterOutput fco = new FuzzyClusterOutput(args[0]);
+	    	fco.doStuff(args[1]);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+        }
+    }
+
+	public FuzzyClusterOutput(String clusteredPointsDir) {
+		inputDir = clusteredPointsDir;
+	}
+
+	public void doStuff(String outputFilePrefix) throws IOException {
+
+        BufferedWriter bw;
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
+        File pointsFolder = new File(inputDir);
+
+        for (File file : pointsFolder.listFiles()) {
+            if(!file.getName().startsWith("part-"))
+                continue;
+            System.out.println("reading file " + file.getAbsolutePath());
+            SequenceFile.Reader reader = new SequenceFile.Reader(fs,  new Path(file.getAbsolutePath()), conf);
+            System.out.println("key is class " + reader.getKeyClassName() + " and value is class " + reader.getValueClassName());
+            IntWritable key = new IntWritable();
+            WeightedPropertyVectorWritable value = new WeightedPropertyVectorWritable();
+
+            // read all the pairs in the sequence file and put them in a map
+            while (reader.next(key, value)) {
+            	//key is the cluster ID, value is the vector mapping clusters to probabilities for this document
+            	String vectorName;
+            	Vector vector = value.getVector();
+            	if (vector instanceof NamedVector) {
+            	    vector = (NamedVector) value.getVector();
+                    vectorName = ((NamedVector) vector).getName();
+            	} else  {
+            		//just so it doesn't break if you didn't use named vectors
+	                vectorName = vector.asFormatString();
+            	}
+            	
+            	//add to map
+            	insertOrAppend(vectorName, key.toString(), value.getWeight());
+            }
+            reader.close();
+        }
+        
+        /** finished reading. now write to file **/
+    	bw = new BufferedWriter(new FileWriter(new File(outputFilePrefix+"_alldocs")));
+        
+        for (String key : pointVectorToClusterProbs.keySet()) {
+        	ArrayList<Pair> clusterProbs = pointVectorToClusterProbs.get(key);
+        	Collections.sort(clusterProbs); //sorts descending by probability
+        	bw.write(key + "\n");
+        	for (Pair cluster : clusterProbs)
+        		bw.write(cluster.word + "\t" + cluster.similarity + "\n");
+        	bw.write("\n"); //finished with one point
+        }
+        bw.flush();
+        bw.close();
+	}
+
+	private void insertOrAppend(String vectorName, String clusterID, double probability) {
+		Pair pair = new Pair(clusterID, probability);
+		if (pointVectorToClusterProbs.containsKey(vectorName)) {
+			pointVectorToClusterProbs.get(vectorName).add(pair);
+		} else {
+			ArrayList<Pair> addme = new ArrayList<Pair>();
+			addme.add(pair);
+			pointVectorToClusterProbs.put(vectorName, addme);
+		}
+	}
+
+	/**
+	 * A string and its number, to make it easier to sort
+	 * Sorts by numeric descending
+	 */
+	private class Pair implements Comparable<Object> {
+		String word;
+		double similarity;
+		
+		Pair (String word, double distance) {
+			this.word = word;
+			this.similarity = distance;
+		}
+
+		public boolean equals(Object other) {
+			if (((Pair) other).word.equals(this.word) && 
+					((Pair) other).similarity == (this.similarity))
+				return true;
+			else
+				return false;
+		}
+		
+		public int hashCode() {
+		    int hash = 1;
+		    hash = hash * 17 + word.hashCode();
+		    hash = hash * 17 + Double.valueOf(similarity).hashCode();
+		    return hash;
+		}
+		
+		//Sorts descending by similarity
+		public int compareTo(Object that) {
+			if (this.similarity < ((Pair)that).similarity)
+				return 1;
+			else if (this.similarity == ((Pair)that).similarity)
+				return 0;
+			else
+				return -1;
+		}
+		
+		public String toString() {
+			return "[" + word + ", " + similarity + "]";
+		}
+	}
+}
---
+++ java/SeedClusterCreator.java
@@ -0,0 +1,156 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
+import org.apache.mahout.clustering.kmeans.Cluster;
+import org.apache.mahout.common.ClassUtils;
+import org.apache.mahout.common.distance.CosineDistanceMeasure;
+import org.apache.mahout.common.distance.DistanceMeasure;
+import org.apache.mahout.math.NamedVector;
+import org.apache.mahout.math.Vector;
+import org.apache.mahout.math.VectorWritable;
+
+import com.google.common.collect.Lists;
+import com.google.common.io.Closeables;
+
+/**
+ * Reads in a SequenceFile of vectors and a file containing docIDs. Creates a
+ * SequenceFile of clusters from the vectors matching the docIDs.
+ * 
+ * @author megs
+ */
+public class SeedClusterCreator {
+
+	private String inputVectorFile;
+	private String seedFile;
+	private Path outputDir;
+	private Set<String> seedNames = new HashSet<String>();
+
+	/**
+	 * @param args
+	 *            - <vector file> <seed id file> <output dir>
+	 */
+	public static void main(String[] args) {
+		if (args.length < 3) {
+			System.out
+					.println("Usage:\njava SeedClusterCreator <vector file> <seed id file> <output dir>");
+			System.exit(1);
+		}
+
+		try {
+			SeedClusterCreator seeder = new SeedClusterCreator(args[0], args[1], args[2]);
+			seeder.doStuff();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public SeedClusterCreator(String vectorFileName, String seedFileName,
+			String outputDirName) {
+		inputVectorFile = vectorFileName;
+		outputDir = new Path(outputDirName);
+		seedFile = seedFileName;
+	}
+
+	public void doStuff() throws IOException {
+		readSeedIDs();
+		readVectorFile();
+	}
+
+	/**
+	 * Reads in a SequenceFile with NamedVectors and finds the seed vectors.
+	 * Converts them to clusters and writes them to a Cluster SequenceFile
+	 * 
+	 * @throws IOException
+	 */
+	private void readVectorFile () throws IOException {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
+        
+        //set up for reading stuff
+        File vectorfile = new File(inputVectorFile);
+        SequenceFile.Reader reader = new SequenceFile.Reader(fs,  new Path(vectorfile.getAbsolutePath()), conf);
+        LongWritable key = new LongWritable();
+        VectorWritable value = new VectorWritable();
+
+        //set up for writing stuff
+		Path outFile = new Path(outputDir, "part-seeds");
+		SequenceFile.Writer writer = SequenceFile.createWriter(fs, conf,
+				outFile, Text.class, Cluster.class);
+		List<Text> chosenTexts = Lists.newArrayListWithCapacity(seedNames.size());
+		List<Cluster> chosenClusters = Lists.newArrayListWithCapacity(seedNames.size());
+
+		//set up stuff for new cluster creation
+        int nextClusterId = 0;
+        //hardcoding, lazy
+        DistanceMeasure measure = ClassUtils.instantiateAs(CosineDistanceMeasure.class.getName(), DistanceMeasure.class);
+
+        int count = 0;
+        // read all the pairs in the sequence file and find the seed vectors
+        while (reader.next(key, value)) {
+        	//key is some kind of ID, value is the named vector holder
+        	String vectorName;
+        	Vector vector = value.get();
+        	if (vector instanceof NamedVector) {
+                vectorName = ((NamedVector) vector).getName();
+        	} else  {
+                continue;
+                //you forgot the -nv flag
+        	}
+        	System.out.println("Found vector " + vectorName);
+        	count++;
+        	if (seedNames.contains(vectorName)) {
+        		System.out.println("Found vector for desired seed " + vectorName);
+
+				Cluster newCluster = new Cluster(vector, nextClusterId++, measure);
+				newCluster.observe(value.get(), 1);
+				Text newText = new Text(key.toString());
+
+				//add to lists, and then we'll write them as key/value pairs all at once
+				chosenTexts.add(newText);
+				chosenClusters.add(newCluster);
+        	}
+        }
+        reader.close();
+        System.out.println("Total count " + count);
+		try {
+	        //we've collected all the vectors for the requested seeds
+	        // if we couldn't find one, complain loudly and refuse to write the cluster file
+	        if (chosenTexts.size() != seedNames.size()) {
+	        	System.err.println("Sorry, I couldn't find all of the seeds you wanted. Giving up and going home.");
+	        	System.exit(1);
+	        }
+			for (int i = 0; i < chosenTexts.size(); i++) {
+				writer.append(chosenTexts.get(i), chosenClusters.get(i));
+			}
+			System.out.println("Wrote " + chosenTexts.size() + " vectors to " + outFile);
+		} finally {
+			Closeables.closeQuietly(writer);
+		}
+    }
+	
+	/**
+	 * Reads a text file. The seed names should match whatever you set as the name for your NamedVectors
+	 * 
+	 * @throws IOException
+	 */
+	private void readSeedIDs() throws IOException {
+		Scanner s = new Scanner(new File(seedFile));
+		while (s.hasNextLine()) {
+			String id = s.nextLine().trim();
+			if (!id.startsWith("#") && !id.isEmpty()) {
+				seedNames.add(id);
+			}
+		}
+		s.close();
+	}
+}
